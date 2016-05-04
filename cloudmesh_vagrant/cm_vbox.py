474eda947ac3fb48cbdc9ef0391c44a817995f28
Gregor von Laszewski <laszewski@gmail.com>
2016-05-04 15:40:11 -0400

    new:usr: work on distribution

--- cloudmesh_vagrant/cm_vbox.py
+++ cloudmesh_vagrant/cm_vbox.py
@@ -1,12 +1,19 @@
-"""Usage:
-  cm vbox version
-  cm vbox image list [--format=FORMAT]
-  cm vbox vm list [--format=FORMAT]
-  cm vbox vm delete NAME
-  cm vbox create NAME ([--memory=MEMORY] [--image=IMAGE] [--script=SCRIPT] | list)
-  cm vbox vm boot NAME ([--memory=MEMORY] [--image=IMAGE] [--script=SCRIPT] | list)
+"""
+::
 
-  cm -h | --help | --version
+    Usage:
+      cm-vbox version
+      cm-vbox image list [--format=FORMAT]
+      cm-vbox vm list [--format=FORMAT]
+      cm-vbox vm delete NAME
+      cm-vbox create NAME ([--memory=MEMORY]
+                           [--image=IMAGE]
+                           [--script=SCRIPT] | list)
+      cm-vbox vm boot NAME ([--memory=MEMORY]
+                            [--image=IMAGE]
+                            [--script=SCRIPT] | list)
+
+      cm -h | --help | --version
 """
 from docopt import docopt
 import cloudmesh_vagrant as vagrant
@@ -14,13 +21,18 @@ from cloudmesh_client.common.dotdict import dotdict
 from pprint import pprint
 from cloudmesh_client.common.Printer import Printer
 from cloudmesh_client.common.Shell import Shell
-
+import sys
 
 # pprint (vagrant.vm.list())
 # vagrant.vm.execute("w2", "uname")
 # pprint (vagrant.image.list())
 
 def defaults():
+    """
+    default values
+    :return: a number of default values for memory, image, and script
+    :rtype: dotdict
+    """
     d = dotdict()
     d.memory = 1024
     d.image = "ubuntu/trusty64"
@@ -29,26 +41,20 @@ def defaults():
 
 
 
-def convert(lst, id="name"):
+def _convert(lst, id="name"):
     d = {}
     for entry in lst:
         d[entry[id]] = entry
     return d
 
 
-# pprint (convert(vagrant.vm.list()))
-# pprint (convert(vagrant.image.list()))
-# vms = convert(vagrant.vm.list())
-# vagrant.vm.execute("w2", "uname")
-
-
-def LIST_PRINT(l, output, order=None):
-    if arg.format in ["yaml", "dict", "json"]:
-        l = convert(l)
+def _LIST_PRINT(l, output, order=None):
+    if output in ["yaml", "dict", "json"]:
+        l = _convert(l)
 
     result = Printer.write(l,
                            order=order,
-                           output=arg.format)
+                           output=output)
 
     if output in ["table", "yaml", "json", "csv"]:
         print(result)
@@ -56,8 +62,11 @@ def LIST_PRINT(l, output, order=None):
         pprint(result)
 
 
-if __name__ == '__main__':
-
+def main():
+    """
+    The main method
+    :return: prints the actions to the terminal
+    """
     arg = dotdict(docopt(__doc__, version='0.1'))
     arg.format = arg["--format"] or "table"
 
@@ -65,11 +74,11 @@ if __name__ == '__main__':
         print(vagrant.version())
     elif arg.image and arg.list:
         l = vagrant.image.list()
-        LIST_PRINT(l, arg.format, order=["name", "provider", "date"])
+        _LIST_PRINT(l, arg.format, order=["name", "provider", "date"])
 
     elif arg.vm and arg.list:
         l = vagrant.vm.list()
-        LIST_PRINT(l,
+        _LIST_PRINT(l,
                    arg.format,
                    order=["name", "state", "id", "provider", "directory"])
 
@@ -114,3 +123,6 @@ if __name__ == '__main__':
     else:
 
         print "use help"
+
+if __name__ == '__main__':
+    main(sys.arg)
